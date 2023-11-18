b109e5e28b210b1b697d88606b880280fe26adaf
I_am_Emmanuel <emmaenwereji@gmail.com>
2023-11-18 21:27:04 +0100

    add index.js file

--- js/index.js
+++ js/index.js
@@ -7,6 +7,16 @@ function hideSidebar(){
     sidebar.style.display = 'none'
 }
 
+let accordions = document.querySelectorAll('.accordion-wrapper .accordion')
+accordions.forEach((acco)=>{
+    acco.onclick = () =>{
+        accordions.forEach((subcontent) =>{
+            subcontent.classList.remove("active");
+        })
+        acco.classList.add("active");
+    }
+})
+
 const swiper = new Swiper('.swiper', {
     autoplay: {
         delay: 3000,
