c143c0d2ef7689b1f40db124cce9d6f8c2945370
I_am_Emmanuel <emmaenwereji@gmail.com>
2023-11-20 10:28:11 +0100

    add js file

--- js/index.js
+++ js/index.js
@@ -17,25 +17,6 @@ accordions.forEach((acco)=>{
     }
 })
 
-const swiper = new Swiper('.swiper', {
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-loop: true,
-
-pagination: {
-el: '.swiper-pagination',
-clickable: true,
-},
-
-navigation: {
-nextEl: '.swiper-button-next',
-prevEl: '.swiper-button-prev',
-},
-
-});
-
 $(".testimonial_slider_area").owlCarousel({
     loop:true,
     items:3,
--- js/portfolio.js
+++ js/portfolio.js
@@ -6,17 +6,3 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
-
-const swiper = new Swiper('.swiper', {
-loop: true,
-
-pagination: {
-el: '.swiper-pagination',
-clickable: true,
-},
-
-navigation: {
-nextEl: '.swiper-button-next',
-prevEl: '.swiper-button-prev',
-},
-});
